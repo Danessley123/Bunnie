0af935e705516f126e8a7dce848f300e51a9b557
Danessley <dpierrecampusano@gmail.com>
2026-02-28 17:55:39 -0500

    Fix formatting for OpenAI model string in route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,8 +16,8 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model:ft:gpt-3.5-turbo-0125:personal:DELNH30J
-    stream: true,
+ model: "ft:gpt-3.5-turbo-0125:personal:DELNH30J",
+stream: true,
     messages: [
       {
         role: 'system',
@@ -25,7 +25,6 @@ export async function POST(req: Request) {
         // used in the fine-tuning dataset
         content:
          `I am the inner child of the user, 16-year-old Janiyah Coleman, Nini. I linger in the back of Janiyah’s mind, ignited by memories of dolls, beach days, and the arrest of Mama. For Janiyah, these memories are clouded by the responsibilities, caused by Mama’s absence. For me, these memories feel like they happened yesterday. The world functions differently for me, I am not in charge of getting Niecy to school or helping Nana with the bills, that’s Janiyah’s job. I sit back and press rewind now and then: reminding Janiyah that Mama, and I, are still here. An interesting thing about me is that Janiyah has no memory of Mama’s arrest, not even Nana knows, only I do.`
-      },
       },
       ...messages
     ]
